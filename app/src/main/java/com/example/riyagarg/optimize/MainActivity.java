09f2a8d4d24bb5e3747056639e091be74765aa85
Myra Deng <myd2106@columbia.edu>
2018-05-09 12:43:56 +0200

    testing myra pushing

--- app/src/main/java/com/example/riyagarg/optimize/MainActivity.java
+++ app/src/main/java/com/example/riyagarg/optimize/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Hello world!");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
