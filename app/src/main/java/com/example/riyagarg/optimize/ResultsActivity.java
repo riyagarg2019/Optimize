612ab12c27bb1f04ce66633931aea3a0543dcbd7
riyagarg <rgarg2015@gmail.com>
2018-05-15 12:51:37 -0400

    additions to results activity, created adapter for results, (navigation drawer commented out)

--- app/src/main/java/com/example/riyagarg/optimize/ResultsActivity.java
+++ app/src/main/java/com/example/riyagarg/optimize/ResultsActivity.java
@@ -8,8 +8,14 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.widget.Toast;
 
 import com.adapter.DestinationRecyclerAdapter;
+import com.adapter.ResultsRecyclerAdapter;
+import com.data.AppDatabase;
 import com.data.Destination;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +28,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.touch.DestinationTouchHelperCallback;
 
 import java.io.IOException;
 import java.util.LinkedList;
@@ -37,6 +44,7 @@ public class ResultsActivity extends AppCompatActivity implements LocationListen
     private List<LatLng> positionList = new LinkedList<>();
     private PolylineOptions polyLineOpts;
     private LatLngBounds.Builder builder = new LatLngBounds.Builder();
+    private ResultsRecyclerAdapter resultsRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +53,9 @@ public class ResultsActivity extends AppCompatActivity implements LocationListen
 
         destinationList = (List<Destination>) getIntent().getSerializableExtra("LIST");
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(ResultsActivity.this);
-        //destinationList = getIntent().getStringArrayExtra(MainActivity.DESTINATION_LIST);
 
+        setRecyclerView();
 
-
-        //mMap = ((MapFragment) getFragmentManager().
-                        //findFragmentById(R.id.map)).getMap(); mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID); mMap.setTrafficEnabled(true);
-
-        /*mMap.addMarker(new MarkerOptions()
-                .position(new LatLng(19, 35))
-                .title("Hello Android team!"));*/
     }
 
     @Override
@@ -84,37 +81,32 @@ public class ResultsActivity extends AppCompatActivity implements LocationListen
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        //destinationList = getParentActivityIntent()
 
 
-        //destinationList = destinationRecyclerAdapter.getDestinationList(); //not a real line
-        /*Destination eger = new Destination("Eger", 47.9, 20.37);
-        Destination budapest = new Destination("Budapest", 47.5, 19.04);
-        Destination bratislava = new Destination("Bratislava", 48.15, 17.11);*/
-
-
-        for(int i = 0; i < destinationList.size(); i++){
-            LatLng position = new LatLng(destinationList.get(i).getLat(), destinationList.get(i).getLng());
-            positionList.add(position);
-            Marker myMarker = mMap.addMarker(new MarkerOptions().position(position).title(destinationList.get(i).getLocation()));
-            markerList.add(myMarker);
-        }
-
-        for (int i = 0; i < destinationList.size(); i++){
-            if (i != destinationList.size()-1) {
-                polyLineOpts = new PolylineOptions().add(positionList.get(i), positionList.get(i + 1)); ////draw lines based on optimized positions
-            } else {
-                polyLineOpts = new PolylineOptions().add(positionList.get(destinationList.size()-1), positionList.get(0));
+        if(destinationList.size() != 0) {
+            for (int i = 0; i < destinationList.size(); i++) {
+                LatLng position = new LatLng(destinationList.get(i).getLat(), destinationList.get(i).getLng());
+                positionList.add(position);
+                Marker myMarker = mMap.addMarker(new MarkerOptions().position(position).title(destinationList.get(i).getLocation()));
+                markerList.add(myMarker);
             }
-            Polyline polyline = mMap.addPolyline(polyLineOpts);
-            polyline.setColor(Color.GREEN);
-            builder.include(positionList.get(i));
-         }
 
-        LatLngBounds bounds = builder.build();
-        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 20));
+            for (int i = 0; i < destinationList.size(); i++) {
+                if (i != destinationList.size() - 1) {
+                    polyLineOpts = new PolylineOptions().add(positionList.get(i), positionList.get(i + 1)); ////draw lines based on optimized positions
+                } else {
+                    polyLineOpts = new PolylineOptions().add(positionList.get(destinationList.size() - 1), positionList.get(0));
+                }
+                Polyline polyline = mMap.addPolyline(polyLineOpts);
+                polyline.setColor(Color.GREEN);
+                builder.include(positionList.get(i));
+            }
 
-        mMap.setTrafficEnabled(true);
+            LatLngBounds bounds = builder.build();
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 20));
+
+            mMap.setTrafficEnabled(true);
+        }
 
 
         /*mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
@@ -165,8 +157,35 @@ public class ResultsActivity extends AppCompatActivity implements LocationListen
 
        // mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
 
-
-
-
     }
+
+    private void setRecyclerView() {
+        RecyclerView recyclerViewResults = findViewById(R.id.recycler);
+        recyclerViewResults.setHasFixedSize(true);
+        recyclerViewResults.setLayoutManager(new LinearLayoutManager(this));
+        initDestinations(recyclerViewResults);
+    }
+
+    public void initDestinations(final RecyclerView recyclerView) {
+        new Thread() {
+            @Override
+            public void run() {
+                final List<Destination> dests = destinationList;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultsRecyclerAdapter = new ResultsRecyclerAdapter(dests, ResultsActivity.this);
+                        recyclerView.setAdapter(resultsRecyclerAdapter);
+                        ItemTouchHelper.Callback callback =
+                                new DestinationTouchHelperCallback(resultsRecyclerAdapter);
+                        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+                        touchHelper.attachToRecyclerView(recyclerView);
+                    }
+                });
+            }
+        }.start();
+    }
+
+
+
 }
